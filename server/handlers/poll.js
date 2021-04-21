4340bb4017841edd0432223db91f9fa455fe5136
daniyaniazi <farihadania@hotmail.com>
2021-04-22 01:36:04 +0500

    Done with backen
    get user polls , delete poll , vote poll

--- server/handlers/poll.js
+++ server/handlers/poll.js
@@ -2,7 +2,7 @@ const db = require('../models')
 
 exports.showPolls = async (req, res, next) => {
     try {
-        const polls = await db.Poll.find();
+        const polls = await db.Poll.find().populate('user', ['username', 'id']);
 
         res.status(200).json(polls)
 
@@ -31,6 +31,104 @@ exports.createPoll = async (req, res, next) => {
         await user.save()
         res.status(201).json({ ...poll._doc, user: user._id })
 
+    } catch (error) {
+        error.status = 400;
+        next(error)
+    }
+}
+
+exports.userPolls = async (req, res, next) => {
+    try {
+        const { id } = req.decoded;
+        const user = await db.User.findById(id).populate('polls')
+        console.log(user)
+        res.status(200).json(user.polls)
+    } catch (error) {
+        error.status = 400;
+        next(error)
+    }
+}
+
+exports.getPoll = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const poll = await db.Poll.findById(id).populate('user', ['username', 'id'])
+
+        if (!poll) throw new Error("Poll Not Found")
+
+        res.status(200).json(poll)
+    } catch (error) {
+        error.status = 400;
+        next(error)
+    }
+}
+
+exports.deletePoll = async (req, res, next) => {
+    try {
+        const { id: pollId } = req.params;
+
+        const { id: userId } = req.decoded
+
+        const poll = await db.Poll.findById(pollId)
+
+        if (!poll) throw new Error("Poll Not Found")
+
+        //poll.user in objectId mongodb type
+        if (poll.user.toString() !== userId) {
+            throw new Error("Unauthorized access")
+        }
+
+        await poll.remove()
+        res.status(202).json(poll)
+    } catch (error) {
+        error.status = 400;
+        next(error)
+    }
+}
+
+exports.vote = async (req, res, next) => {
+    try {
+        const { id: pollId } = req.params;
+
+        const { id: userId } = req.decoded
+
+        const { answer } = req.body;
+        if (answer) {
+
+            const poll = await db.Poll.findById(pollId)
+            if (!poll) throw new Error("Poll Not Found")
+
+            //voting
+            const vote = poll.options.map(option => {
+                console.log("here:", option.option)
+                if (option.option === answer) {
+                    return {
+                        option: option.option,
+                        _id: option._id,
+                        votes: option.votes + 1
+
+                    };
+
+                }
+                else {
+                    return option
+                }
+            })
+
+            //check user vote and mark
+            if (poll.voted.filter(user => user.toString() === userId) <= 0) {
+                poll.voted.push(userId);
+                poll.options = vote;
+                await poll.save();
+                res.status(202).json(poll)
+            } else {
+                throw new Error("Already Voted")
+
+            }
+
+        } else {
+            throw new Error("No answer provided")
+        }
     } catch (error) {
         error.status = 400;
         next(error)
--- server/routes/poll.js
+++ server/routes/poll.js
@@ -5,4 +5,11 @@ const auth = require('../middlewares/auth')
 router.route('/').get(handle.showPolls)
     .post(auth, handle.createPoll)
 
+router.get('/user', auth, handle.userPolls)
+
+router.route('/:id')
+    .get(handle.getPoll)
+    .post(auth, handle.vote)
+    .delete(auth, handle.deletePoll)
+
 module.exports = router;
