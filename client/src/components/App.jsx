3415c4ab998fb3b4013d2fbefed49ad59ca364c4
daniyaniazi <farihadania@hotmail.com>
2021-04-22 21:14:34 +0500

    Api connected

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,20 @@
-import { React } from "react";
+import React, { Component } from "react";
+import api from '../services/api'
 
-const App = () => <div>App works</div>
 
+class App extends Component {
+
+    //function run mmediiately after compoent mount
+    async componentDidMount() {
+        const result = await api.call('post', "auth/login", {
+            username: 'username',
+            password: "password"
+        })
+
+        console.log("res:", result)
+    }
+    render() {
+        return <div>App works here</div>
+    }
+}
 export default App;
---
+++ client/src/services/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+export const setToken = token => {
+    if (token) {
+        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+    }
+    else {
+        delete axios.defaults.headers.common['Authorization']
+    }
+}
+
+export const call = async (method, path, data) => {
+    //axios.post/get
+    const response = await axios[method](`/${path}`, data);
+    return response.data;
+}
+
+export default { call, setToken };
